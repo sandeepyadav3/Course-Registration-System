74d3d5b4ea91634fe237af4039f161ffcf9d2d29
sandeepyadav3 <sandeepyadavps4@gmail.com>
2022-04-24 12:52:31 -0500

    Initial commit

---
+++ courseinfo/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from.models import Period, Year, Semester, Course, Instructor, Student, Section, Registration
+
+admin.site.register(Period)
+admin.site.register(Year)
+admin.site.register(Semester)
+admin.site.register(Course)
+admin.site.register(Instructor)
+admin.site.register(Student)
+admin.site.register(Section)
+admin.site.register(Registration)
